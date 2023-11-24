ac3a28fbd58a4ef3cd33b102aeb2c3c99d9fc088
Bang Hyo Bin <bin7719@daum.net>
2023-11-24 13:11:02 +0900

    first commit

---
+++ src/main/java/com/example/MovingMate/MovingMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.MovingMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovingMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovingMateApplication.class, args);
+	}
+
+}
